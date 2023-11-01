f12c04d9c44f01a128d7109f70dd9422bd1e6f18
Julius Koskela <me@juliuskoskela.dev>
2023-11-01 17:48:05 +0200

    Add default.nix to .cheats
    Signed-off-by: Julius Koskela <me@juliuskoskela.dev>

---
+++ .cheat/derivation/ex00/default.nix
@@ -0,0 +1,13 @@
+{ stdenv }:
+stdenv.mkDerivation rec {
+  pname = "foo";
+  version = "1.0";
+  src = ./.;
+  buildPhase = ''
+    $CC -o foo foo.c
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp foo $out/bin/foo
+  '';
+}
---
+++ .cheat/derivation/ex01/default.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation rec {
+  pname = "tinychess";
+  version = "1.0";
+
+  src = pkgs.fetchgit {
+    url = "https://github.com/zwegner/tinychess.git";
+    rev = "5a3395a";
+    hash = "sha256-sTQr8n72P4EQ8pvMpZzUAzFIWqUpoG8CduHllLo8LGA=";
+  };
+
+  buildPhase = "$CC tinychess.c -o tinychess";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp tinychess $out/bin/tinychess
+  '';
+
+  meta = with pkgs.lib; {
+    description = "TinyChess: A chess engine in less than 1,000 lines of C";
+    license = licenses.mit;
+  };
+}
